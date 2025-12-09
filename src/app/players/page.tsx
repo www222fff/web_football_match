7ee4e388ccb4afbc673fbf00413400cab9bcf62d
Danny Wang <feng73440@gmail.com>
2025-12-09 11:16:27 +0000

    你尽快改吧

--- src/app/players/page.tsx
+++ src/app/players/page.tsx
@@ -8,7 +8,7 @@ export async function generateStaticParams() {
   const editions = await getAvailableEditions();
   const params = editions.map(e => ({ edition: e.id }));
   // Add a param for the default route (no query params)
-  params.push({ edition: undefined! });
+  params.push({} as any);
   return params;
 }
 
