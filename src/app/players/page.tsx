b4c4df98492312b38906c2892a7f63b06c2078b9
Danny Wang <feng73440@gmail.com>
2025-12-09 11:21:02 +0000

    你是要删除   params.push({} as any); ？

--- src/app/players/page.tsx
+++ src/app/players/page.tsx
@@ -8,9 +8,8 @@ export async function generateStaticParams() {
   const editions = await getAvailableEditions();
   const params = editions.map(e => ({ edition: e.id }));
   
-  // Add a param for the default route (no query params)
-  // This explicitly tells Next.js to also build the `/players` route.
-  return [...params, {}];
+  // Add an entry for the default path with no search params
+  return [{}, ...params];
 }
 
 export default async function PlayersPage({ searchParams }: { searchParams: { edition?: string } }) {
