0035af1bfae47b161897d8851d629532774e74b4
Danny Wang <feng73440@gmail.com>
2025-12-12 23:48:13 +0000

    当前主题太暗，尝试修改一个更清爽，亮一点主题，你有什么建议吗

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,7 +14,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="zh-CN" className='dark'>
+    <html lang="zh-CN">
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
